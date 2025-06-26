5cd1f74d48a04454fef3b9cccaf852bb73e40a0c
IvoRubino <123120319+IvoRubino@users.noreply.github.com>
2025-06-25 22:29:18 -0300

    Add files via upload

---
+++ TPPDSG9/src/main/java/com/uade/procesoyDesarrolloDeSoftware/TPPDSG9/Tppdsg9Application.java
@@ -0,0 +1,13 @@
+package com.uade.procesoyDesarrolloDeSoftware.TPPDSG9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tppdsg9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tppdsg9Application.class, args);
+	}
+
+}
---
+++ TPPDSG9/src/test/java/com/uade/procesoyDesarrolloDeSoftware/TPPDSG9/Tppdsg9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.procesoyDesarrolloDeSoftware.TPPDSG9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tppdsg9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
